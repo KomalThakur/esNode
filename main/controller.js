93efd2b42e724965223e9a0442487ac11bf6bc3d
Komal Thakur <komalthakur@Komals-MacBook-Air.local>
2018-08-01 21:52:25 +0530

    express changes

--- main/controller.js
+++ main/controller.js
@@ -2,12 +2,13 @@ const esService = require('./service');
 
 async function getUniqueSessions(req, res, next) {
 	try {
-		console.log(req.header);
+		
 		const sessions = await esService.getUniqueSessions(req);
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		//res.sendStatus(200);
-		res.send(sessions);
+		
+		console.log("controller sessions : ", sessions);
+		res.status(200).send(sessions);
 
 	} catch (error) {
 		next(error);
