3cc0edfdbb52862e97d40b5d027cc25fe7dda6e5
Komal Thakur <komalthakur@Komals-MacBook-Air.local>
2018-08-01 21:17:28 +0530

    respinse changes

--- main/controller.js
+++ main/controller.js
@@ -3,11 +3,11 @@ const esService = require('./service');
 async function getUniqueSessions(req, res, next) {
 	try {
 		console.log(req.header);
-		await esService.getUniqueSessions(req);
+		const sessions = await esService.getUniqueSessions(req);
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		res.status(200);
-		res.send('success');
+		res.send(sessions);
 
 	} catch (error) {
 		next(error);
